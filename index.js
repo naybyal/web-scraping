aa6a95da0d136a2af386c2fb1d54901d4f4a5dac
naybyal <officiallynabiel@gmail.com>
2024-04-06 19:05:16 +0530

    Scraped goodreads.com

--- index.js
+++ index.js
@@ -1,17 +1,35 @@
 const PORT = 8000;
 
-import express from 'express';
-import axios from 'axios';
-import cheerio from 'cheerio';
+const express = require('express');
+const axios = require('axios');
+const cheerio = require('cheerio');
 
 const app = express();
 
 
 app.get('/', async (req, res) => {
-    res.json({ message: 'Web Scraping 101' });
+    res.json({ message: 'Scraping Goodreads' });
 });
 
 // Get data from website using URL
+
+app.get('/goodreads', async (req, res) => {
+    axios.get('https://www.goodreads.com/list/show/1230.Best_Gothic_Books_Of_All_Time')
+    .then(response => {
+        const html = response.data;
+        const $ = cheerio.load(html);
+        const books = [];
+        $('.bookTitle').each((i, elem) => {
+            books[i] = {
+                title: $(elem).text(),
+                url: $(elem).attr('href')
+            };
+        });
+        console.log(books);
+        res.json(books);
+    })
+});
+
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
 
